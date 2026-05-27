dec9ba0cbfdf1816e939b4272dd906315cd883a0
Ashutosh <ashu57093@gmail.com>
2026-05-27 21:43:44 +0530

    feed page and build a feature send the accept request and reject request

--- devTinder-web/src/components/UserCard.jsx
+++ devTinder-web/src/components/UserCard.jsx
@@ -1,22 +1,56 @@
+import axios from "axios";
+import {BASE_URL} from "../utils/constants"
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 const UserCard = ({user})=>{
-    const {firstName, lastName, photoUrl,age,gender,about} = user
+
+    const {_id,firstName, lastName, photoUrl,age,gender,about} = user
     console.log(user)
+    const dispatch = useDispatch();
+
+    const handleSendRequest = async (status,userId) =>{
+      try{
+        const res = await axios.post(BASE_URL + "/request/send/" + status + "/"+ userId, {} , {withCredentials:true});
+         dispatch(removeUserFromFeed(userId))
+      }catch(err){
+
+      }
+    }
+
+
     return (
       <div className="card bg-base-300 w-96 shadow-sm ">
         <figure>
           {/* <img src={user.photoUrl} alt="user photo" /> */}
-          <img className=" p-6 w-70 h-70 "
+          <img
+            className=" p-6 w-70 h-70 "
             src="https://img.magnific.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740&q=80"
             alt="user photo"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title justify-center text-3xl">{firstName + " " + lastName}</h2>
-          {age && gender && <p className="flex justify-center text-2xl">{age + ", " + gender}</p>}
+          <h2 className="card-title justify-center text-3xl">
+            {firstName + " " + lastName}
+          </h2>
+          {age && gender && (
+            <p className="flex justify-center text-2xl">
+              {age + ", " + gender}
+            </p>
+          )}
           <p className="flex justify-center">{about}</p>
           <div className="card-actions justify-center my-4">
-            <button className="btn btn-primary">Ignore</button>
-            <button className="btn btn-secondary">Interested</button>
+            <button
+              className="btn btn-primary"
+              onClick={()=> handleSendRequest("ignored", _id)}
+            >
+              Ignore
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={()=> handleSendRequest("interested", _id)}
+            >
+              Interested
+            </button>
           </div>
         </div>
       </div>
